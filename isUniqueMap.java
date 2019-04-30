1b8bfc955fbc63a83114f6ade5d2ed6483bd85ea
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2019-04-30 14:46:18 -0700

    Add files via upload

---
+++ isUniqueMap.java
@@ -0,0 +1,27 @@
+//write a method isUnique that accepts a map whose keys and val r strings as a parameters
+//and returns true if no 2 key map to the same value and 
+//false if 2 or more keys do map to the same value.
+import java.util.HashMap;
+import java.util.Map;
+
+public class isUniqueMap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		isUnique("abc","123");
+		
+		
+	}
+	public static boolean isUnique(String string, String string2) {
+		// TODO Auto-generated method stub
+		Map<String, String> pary = new HashMap<String, String>();
+		 
+		
+		if((String string2 : pary) < 1){
+			return true;
+		} else 
+		
+			return false;
+	}
+	
+}
