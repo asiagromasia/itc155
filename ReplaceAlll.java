80d56c6c092bcfb6911fe35fec442008002b4e06
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2019-06-10 14:17:42 -0700

    split methods

--- ReplaceAlll.java
+++ ReplaceAlll.java
@@ -1,26 +1,20 @@
-import java.sql.Array;
-
 //Write a method called replaceAll that accepts two integer values as parameter and replaces
 //all occurences of the first value in the list with the second value
 public class ReplaceAlll {
 
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] data = {11, -7, 3, 42, 3, 0, 14, 3};
-		ArrayIntList list = new ArrayIntList();
-		list.replaceAll(3, 999); 
-		System.out.println(list);
+		int[] list1 = {11, -7, 3, 42, 3, 0, 14, 3};
+	//	System.out.println("old list" + (Arrays.toString(list1)));
+		ArrayIntList list2 = new ArrayIntList();
+		
+		 list2.replaceAll(3,999); 
+	
+	
 	}
 	
-	public void replaceAll(int value, int newValue) {
-		int[] data = {11, -7, 3, 42, 3, 0, 14, 3};
-		ArrayIntList list = new ArrayIntList();
-		 for (int n : data) {
-			 n=newValue;
-	        
-			 System.out.println(list);
-	        }
-		 System.out.println(list);
-	}
 }
+// needs to have methods in seperate file
+	
